8b07d5ec5a237874e132728daef8be9c5082ff82
MinhQuan <minhquan23102000@gmail.com>
2021-11-03 12:13:06 +0700

    Fix requirement.txt

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from website import create_app
 import os
-from chatbot import bot
 
 app = create_app()
-port = int(os.environ.get('PORT', 5000))
 
 if __name__ == '__main__':
-    #bot.__train__()
-    app.run(debug=True, port=port)
+    app.run(debug=True)
