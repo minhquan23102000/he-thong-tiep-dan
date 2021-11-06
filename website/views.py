4b1fa2ce2d6a66e3bf1e902e4fd8f8d8c3cf8c40
MinhQuan <minhquan23102000@gmail.com>
2021-11-06 11:46:42 +0700

    Fix slugsize is to large issue

--- website/views.py
+++ website/views.py
@@ -30,19 +30,19 @@ def chatbot_reponse(msg: str):
    
 
     #Google search this paper if bot doesnt know about it
-    words = ['thủ tục', 'hành chính', 'giấy tờ', 'đơn', 'giấy phép', 'đăng ký']
-    if reponse == bot.DEFAULT_REPONSE:
-        from underthesea import pos_tag
-        tags = pos_tag(msg)
-        if any(w[0].lower() in words for w in tags):
-            print("asdassd")
-            from googlesearch import search
-            # Make a request to google search
-            try:
-                url = list(search(msg, tld='com', lang='vi', num=1, stop=1, pause=2, country='vi'))[0]
-                reponse = f'{bot.DEFAULT_REPONSE} Nhưng mình nghĩ bạn có thể tham khảo thêm tại đây: {url}'
-            except Exception as e:
-                reponse = bot.DEFAULT_REPONSE
-                print(e.__traceback__)
+    # words = ['thủ tục', 'hành chính', 'giấy tờ', 'đơn', 'giấy phép', 'đăng ký']
+    # if reponse == bot.DEFAULT_REPONSE:
+    #     from underthesea import pos_tag
+    #     tags = pos_tag(msg)
+    #     if any(w[0].lower() in words for w in tags):
+    #         print("asdassd")
+    #         from googlesearch import search
+    #         # Make a request to google search
+    #         try:
+    #             url = list(search(msg, tld='com', lang='vi', num=1, stop=1, pause=2, country='vi'))[0]
+    #             reponse = f'{bot.DEFAULT_REPONSE} Nhưng mình nghĩ bạn có thể tham khảo thêm tại đây: {url}'
+    #         except Exception as e:
+    #             reponse = bot.DEFAULT_REPONSE
+    #             print(e.__traceback__)
 
     return reponse
