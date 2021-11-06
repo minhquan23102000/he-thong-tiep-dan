55ab37bf0a762591fd4c7676e04fa2fc388e6650
MinhQuan <minhquan23102000@gmail.com>
2021-11-06 12:05:53 +0700

    Fix slugsize is to large issue

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash
 from chatbot import bot
-#import langid
+import langid
 
 views = Blueprint('views', __name__)
 Sonny = bot.Sonny
@@ -18,8 +18,8 @@ def get_bot_response():
 
 def chatbot_reponse(msg: str):
     #Check if not vietnamese langue
-    # if langid.classify(msg)[0] != 'vi':
-    #     return bot.NOT_VIETNAMESE_LANGUAGE_REPONSE
+    if langid.classify(msg)[0] != 'vi':
+        return bot.NOT_VIETNAMESE_LANGUAGE_REPONSE
 
     #Get reponse from bot
     reponse = Sonny.get_response(msg)
@@ -30,19 +30,19 @@ def chatbot_reponse(msg: str):
    
 
     #Google search this paper if bot doesnt know about it
-    # words = ['thủ tục', 'hành chính', 'giấy tờ', 'đơn', 'giấy phép', 'đăng ký']
-    # if reponse == bot.DEFAULT_REPONSE:
-    #     from underthesea import pos_tag
-    #     tags = pos_tag(msg)
-    #     if any(w[0].lower() in words for w in tags):
-    #         print("asdassd")
-    #         from googlesearch import search
-    #         # Make a request to google search
-    #         try:
-    #             url = list(search(msg, tld='com', lang='vi', num=1, stop=1, pause=2, country='vi'))[0]
-    #             reponse = f'{bot.DEFAULT_REPONSE} Nhưng mình nghĩ bạn có thể tham khảo thêm tại đây: {url}'
-    #         except Exception as e:
-    #             reponse = bot.DEFAULT_REPONSE
-    #             print(e.__traceback__)
+    words = ['thủ tục', 'hành chính', 'giấy tờ', 'đơn', 'giấy phép', 'đăng ký']
+    if reponse == bot.DEFAULT_REPONSE:
+        from underthesea import pos_tag
+        tags = pos_tag(msg)
+        if any(w[0].lower() in words for w in tags):
+            print("asdassd")
+            from googlesearch import search
+            # Make a request to google search
+            try:
+                url = list(search(msg, tld='com', lang='vi', num=1, stop=1, pause=2, country='vi'))[0]
+                reponse = f'{bot.DEFAULT_REPONSE} Nhưng mình nghĩ bạn có thể tham khảo thêm tại đây: {url}'
+            except Exception as e:
+                reponse = bot.DEFAULT_REPONSE
+                print(e.__traceback__)
 
     return reponse
