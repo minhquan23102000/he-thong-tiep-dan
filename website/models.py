7a0ed934b241db22fc944018abde991e8f441dac
MinhQuan <minhquan23102000@gmail.com>
2021-11-08 19:35:00 +0700

    adding model for flask app

--- website/models.py
+++ website/models.py
@@ -1,15 +1,36 @@
-from chatbot.bot import Sonny
 from chatterbot import constants
-from sqlalchemy import Table, Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from . import db
+from chatterbot.ext.sqlalchemy_app.models import Statement, Tag
+from flask_security import Security, SQLAlchemyUserDatastore, \
+    UserMixin, RoleMixin, login_required, current_user
 
+# Define models
+roles_users = db.Table(
+    'roles_users',
+    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+)
 
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
 
-class MyStatement(db.Model, Sonny.storage.get_statement_model()):
-    def __init__(self, *args, **kwargs):
-        super(Sonny.storage.get_statement_model(), self).__init__(*args, **kwargs)
+    def __str__(self):
+        return self.name
 
-class MyTag(db.Model, Sonny.storage.get_tag_model()):
-    pass
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(255), nullable=False)
+    last_name = db.Column(db.String(255))
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    active = db.Column(db.Boolean(), default = False)
+    create_at = db.Column(db.DateTime(timezone=True), default=func.now())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+    def __str__(self):
+        return self.username
