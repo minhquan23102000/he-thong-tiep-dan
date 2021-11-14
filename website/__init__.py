c6c5131520b00adb11a2cd1f4e574414a5c46240
MinhQuan <minhquan23102000@gmail.com>
2021-11-14 08:32:34 +0700

    Adding unknown statement model to store unlearned statement from chatbot

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 import os.path as op
 from flask_admin import Admin
 from flask_admin.contrib.fileadmin import FileAdmin
+from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
 import numpy as np
 
@@ -18,27 +19,36 @@ def create_app():
     #Create database
     db.init_app(app)
 
-    
-
    # Retrain chatbot    
-    from chatbot import bot
-    check = ""
-    while (check != 'Y' and check != 'N'):
-        check = input("Train lại chatbot? Y:N\n")
-        if (check == "Y"):
-            bot.Sonny.storage.drop()
-            bot.__train__()
+    # from chatbot import bot
+    # check = ""
+    # while (check != 'Y' and check != 'N'):
+    #     check = input("Train lại chatbot? Y:N\n")
+    #     if (check == "Y"):
+    #         bot.Sonny.storage.drop()
+    #         bot.__train__()
 
-
-
+    #Import model
+    from .models import UnknownStatement
+    #init_database(app)
+    
     #User setting
     from .views import views
     app.register_blueprint(views, url_prefix='/')
-
 
     #Admin setting
     admin = Admin(app, name = "Hệ thống tiếp dân thông minh", template_mode='bootstrap4')
     path = op.join(op.dirname(app.instance_path), 'chatbot/corpus')
     admin.add_view(FileAdmin(path, '/bot-train-data/', name='Bot Train Files'))
-
+    admin.add_view(ModelView(UnknownStatement, db.session, name='Unknown Sentences'))
+    
     return app
+
+
+def init_database(app):
+    
+    check = ""
+    while (check != 'Y' and check != 'N'):
+        check = input("tạo database? Y:N\n")
+        if (check == "Y"):
+            db.create_all(app=app)
--- website/models.py
+++ website/models.py
@@ -3,34 +3,17 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from . import db
 from chatterbot.ext.sqlalchemy_app.models import Statement, Tag
-from flask_security import Security, SQLAlchemyUserDatastore, \
-    UserMixin, RoleMixin, login_required, current_user
 
-# Define models
-roles_users = db.Table(
-    'roles_users',
-    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
-)
-
-class Role(db.Model, RoleMixin):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True)
-    description = db.Column(db.String(255))
-
-    def __str__(self):
-        return self.name
-
-class User(db.Model, UserMixin):
+class Base(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(255), nullable=False)
-    last_name = db.Column(db.String(255))
-    username = db.Column(db.String(255), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    active = db.Column(db.Boolean(), default = False)
+    
+    
+class UnknownStatement(Base):
+    __tablename__ = 'unknowstatement'
+    question = db.Column(db.String(2000), nullable=False)
+    answer = db.Column(db.String(2000))
     create_at = db.Column(db.DateTime(timezone=True), default=func.now())
-    roles = db.relationship('Role', secondary=roles_users,
-                            backref=db.backref('users', lazy='dynamic'))
-
-    def __str__(self):
-        return self.username
+    isTrain = db.Column(db.Boolean(), default=False)
+    
+    def __repr__(self) -> str:
+        return f'<UnknowStatement {self.question}>'    
