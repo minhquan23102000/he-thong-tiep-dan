122e1b985c855f65a1bd8135b83a519b4e9fc672
MinhQuan <minhquan23102000@gmail.com>
2021-12-12 17:34:13 +0700

    Thêm lớp kế thừa template, cho dễ xây dựng layout

--- website/static/js/chatbox.js
+++ website/static/js/chatbox.js
@@ -1,16 +1,15 @@
-$(function () {
+$(function() {
   var ISSPEECH = false;
   class SpeechRecognitionApi {
     constructor(options) {
       const SpeechToText =
-        window.speechRecognition || window.webkitSpeechRecognition;
+          window.speechRecognition || window.webkitSpeechRecognition;
       this.speechApi = new SpeechToText();
       this.speechApi.continuous = false;
       this.speechApi.interimResults = false;
       this.speechApi.lang = "vi-VN";
-      this.output = options.output
-        ? options.output
-        : document.createElement("div");
+      this.output =
+          options.output ? options.output : document.createElement("div");
       this.speechApi.onresult = (event) => {
         console.log(event);
         var resultIndex = event.resultIndex;
@@ -21,12 +20,12 @@ $(function () {
         send_message(transcript);
       };
 
-      this.speechApi.onstart = function () {
+      this.speechApi.onstart = function() {
         $("#speech-icon").text("mic_none");
         ISSPEECH = true;
       };
 
-      this.speechApi.onspeechend = function () {
+      this.speechApi.onspeechend = function() {
         $("#speech-icon").text("mic_off");
         ISSPEECH = false;
       };
@@ -36,31 +35,27 @@ $(function () {
         ISSPEECH = false;
         generate_message("Bạn có thể nói rõ hơn được không?", 'user');
         this.stop();
-      }
+      };
       this.speechApi.onaudioend = function() {
         $("#speech-icon").text("mic_off");
         ISSPEECH = false;
-      }
+      };
     }
-    init() {
-      this.speechApi.start();
-    }
-    stop() {
-      this.speechApi.stop();
-    }
+    init() { this.speechApi.start(); }
+    stop() { this.speechApi.stop(); }
   }
 
   var INDEX = 1;
-  $("#chat-submit").click(function (e) {
+  $("#chat-submit").click(function(e) {
     e.preventDefault();
     var msg = $("#chat-input").val();
     send_message(msg);
   });
 
-  //Tag change event
-  $("#tag").change(function () {
+  // Tag change event
+  $("#tag").change(function() {
     var newtag = $("#tag").text();
-    $.get("/get-img", { tag: newtag }).done(function (data) {
+    $.get("/get-img", {tag : newtag}).done(function(data) {
       $("#prepare-paper").empty();
       $("#to-khai").empty();
       if (Object.keys(data).length === 0) {
@@ -73,14 +68,21 @@ $(function () {
         if (element.title == "to_khai") {
           tokhai += "<div class='media full'> ";
           tokhai += "<div class = 'layer'>";
-          tokhai += "<p>" + "Tờ khai " + newtag + "</p>" + "</div>";
-          tokhai += "<img " + "src='" + element.src + "' alt='' />" + "</div>";
+          tokhai += "<p>" +
+                    "Tờ khai " + newtag + "</p>" +
+                    "</div>";
+          tokhai += "<img " +
+                    "src='" + element.src + "' alt='' />" +
+                    "</div>";
           tokhai += "\n";
         } else {
           str += "<div class='media'> ";
           str += "<div class = 'layer'>";
-          str += "<p>" + element.title + "</p>" + "</div>";
-          str += "<img " + "src='" + element.src + "' alt='' />" + "</div>";
+          str += "<p>" + element.title + "</p>" +
+                 "</div>";
+          str += "<img " +
+                 "src='" + element.src + "' alt='' />" +
+                 "</div>";
           str += "\n";
         }
       }
@@ -91,13 +93,13 @@ $(function () {
       var element = document.querySelectorAll("img");
       Intense(element);
 
-      $(".layer").on("click", function () {
+      $(".layer").on("click", function() {
         var oldtag = $("#tag").text();
         var msg = $(this).text();
         console.log(msg);
         generate_message(msg, "self");
 
-        $.get("/get", { msg: msg, oldtag: oldtag }).done(function (data) {
+        $.get("/get", {msg : msg, oldtag : oldtag}).done(function(data) {
           var response = linkify(String(data.response));
 
           text2Speech.text = clean_url(response);
@@ -115,7 +117,7 @@ $(function () {
   // Speech recoginition
   var speech = new SpeechRecognitionApi({});
 
-  $("#speech").click(function (e) {
+  $("#speech").click(function(e) {
     e.preventDefault();
 
     if (!ISSPEECH) {
@@ -144,19 +146,17 @@ $(function () {
     str += "          </div>";
     str += "        </div>";
     $(".chat-logs").append(str);
-    $("#cm-msg-" + INDEX)
-      .hide()
-      .fadeIn(300);
+    $("#cm-msg-" + INDEX).hide().fadeIn(300);
     if (type == "self") {
       $("#chat-input").val("");
     }
     $(".chat-logs")
-      .stop()
-      .animate({ scrollTop: $(".chat-logs")[0].scrollHeight }, 1200);
+        .stop()
+        .animate({scrollTop : $(".chat-logs")[0].scrollHeight}, 1200);
   }
 
   function generate_button_message(msg, buttons) {
-    /* Buttons should be object array 
+    /* Buttons should be object array
         [
           {
             name: 'Existing User',
@@ -169,22 +169,19 @@ $(function () {
         ]
       */
     INDEX++;
-    var btn_obj = buttons
-      .map(function (button) {
-        return (
-          '              <li class="button"><a href="javascript:;" class="btn btn-primary chat-btn" chat-value="' +
-          button.value +
-          '">' +
-          button.name +
-          "</a></li>"
-        );
-      })
-      .join("");
+    var btn_obj =
+        buttons
+            .map(function(button) {
+              return (
+                  '              <li class="button"><a href="javascript:;" class="btn btn-primary chat-btn" chat-value="' +
+                  button.value + '">' + button.name + "</a></li>");
+            })
+            .join("");
     var str = "";
     str += "<div id='cm-msg-" + INDEX + '\' class="chat-msg user">';
     str += '          <span class="msg-avatar">';
     str +=
-      '            <img src="https://image.crisp.im/avatar/operator/196af8cc-f6ad-4ef7-afd1-c45d5231387c/240/?1483361727745">';
+        '            <img src="https://image.crisp.im/avatar/operator/196af8cc-f6ad-4ef7-afd1-c45d5231387c/240/?1483361727745">';
     str += "          </span>";
     str += '          <div class="cm-msg-text">';
     str += msg;
@@ -196,28 +193,26 @@ $(function () {
     str += "          </div>";
     str += "        </div>";
     $(".chat-logs").append(str);
-    $("#cm-msg-" + INDEX)
-      .hide()
-      .fadeIn(300);
+    $("#cm-msg-" + INDEX).hide().fadeIn(300);
     $(".chat-logs")
-      .stop()
-      .animate({ scrollTop: $(".chat-logs")[0].scrollHeight }, 1000);
+        .stop()
+        .animate({scrollTop : $(".chat-logs")[0].scrollHeight}, 1000);
     $("#chat-input").attr("disabled", true);
   }
 
-  $(document).delegate(".chat-btn", "click", function () {
+  $(document).delegate(".chat-btn", "click", function() {
     var value = $(this).attr("chat-value");
     var name = $(this).html();
     $("#chat-input").attr("disabled", false);
     generate_message(name, "self");
   });
 
-  $("#chat-circle").click(function () {
+  $("#chat-circle").click(function() {
     $("#chat-circle").toggle("scale");
     $(".chat-box").toggle("scale");
   });
 
-  $(".chat-box-toggle").click(function () {
+  $(".chat-box-toggle").click(function() {
     $("#chat-circle").toggle("scale");
     $(".chat-box").toggle("scale");
   });
@@ -227,17 +222,13 @@ $(function () {
     if (msg.trim()) {
       generate_message(msg, "self");
 
-      $.get("/get", { msg: msg, oldtag: oldtag }).done(function (data) {
+      $.get("/get", {msg : msg, oldtag : oldtag}).done(function(data) {
         console.log(data);
-        //myData = JSON.parse(data)
+        // myData = JSON.parse(data)
         var response = linkify(String(data.response));
         var tag = data.tag;
-        if (
-          tag != "lời chào" &&
-          tag != "cảm xúc" &&
-          tag != oldtag &&
-          tag != "none"
-        ) {
+        if (tag != "lời chào" && tag != "cảm xúc" && tag != oldtag &&
+            tag != "none") {
           $("#tag").text(tag).trigger("change");
         }
 
@@ -252,32 +243,27 @@ $(function () {
   function linkify(inputText) {
     var replacedText, replacePattern1, replacePattern2, replacePattern3;
 
-    //URLs starting with http://, https://, or ftp://
+    // URLs starting with http://, https://, or ftp://
     replacePattern1 =
-      /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-    replacedText = inputText.replace(
-      replacePattern1,
-      '<a href="$1" target="_blank">$1</a>'
-    );
+        /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = inputText.replace(replacePattern1,
+                                     '<a href="$1" target="_blank">$1</a>');
 
-    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    // URLs starting with "www." (without // before it, or it'd re-link the ones
+    // done above).
     replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
     replacedText = replacedText.replace(
-      replacePattern2,
-      '$1<a href="http://$2" target="_blank">$2</a>'
-    );
+        replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
 
-    //Change email addresses to mailto:: links.
+    // Change email addresses to mailto:: links.
     replacePattern3 =
-      /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
-    replacedText = replacedText.replace(
-      replacePattern3,
-      '<a href="mailto:$1">$1</a>'
-    );
+        /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText =
+        replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
 
     return replacedText;
   }
-  function clean_url(inputText){
+  function clean_url(inputText) {
     inputText = String(inputText)
     return inputText.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
   }
